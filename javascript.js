0eadee91d429e74a6980186458be965bc0ddc251
Prasoon <prasoonmaurya813@gmail.com>
2023-08-30 00:57:31 +0530

    Initial Commit (Basic Portfolio created in 2021)                             -----> v1.0.0

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let processScroll = () => {
+  let docElem = document.documentElement,
+    docBody = document.body
+
+  let scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+    scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight
+
+  let scrollPercent = scrollTop / scrollBottom * 100 + '%';
+
+  document.getElementById("progress-bar").style.setProperty("--netWidth", scrollPercent);
+}
+document.addEventListener('scroll', processScroll);
+
+
+// let showSearch = () =>{
+//   let sb =document.getElementById("searchbox")
+//   sb.style.classList.remove('dis-none');
+//   sb.style.classList.add('dis-static');
+// }
+// document.getElementById("sicon").addEventListener('click', showSearch);
